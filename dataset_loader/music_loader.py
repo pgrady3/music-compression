a34b313b166400d44eb391b065a1b86ab090c9a9
Ayush Baid <ayushrakeshbaid@gmail.com>
2019-10-31 14:20:55 -0400

    Using direct mp3 files on the dataloader

--- dataset_loader/music_loader.py
+++ dataset_loader/music_loader.py
@@ -3,9 +3,12 @@ Script with Pytorch's dataloader class
 '''
 import os
 import glob
+import random
 
 import torch
 import torch.utils.data as data
+
+from utils.io import read_mp3
 
 
 class MusicLoader(data.Dataset):
@@ -16,32 +19,26 @@ class MusicLoader(data.Dataset):
   train_folder = 'train'
   test_folder = 'test'
 
-  def __init__(self, root_dir, split='train', transform=None):
+  def __init__(self, root_dir, split='train', snippet_size=65536, transform=None):
     self.root = os.path.expanduser(root_dir)
     self.transform = transform
     self.split = split
+
+    self.snippet_size = snippet_size
 
     if split == 'train':
       self.curr_folder = self.train_folder
     elif split == 'test':
       self.curr_folder = self.test_folder
 
-    self.data_concat = torch.load(
-        os.path.join(
-            root_dir, self.curr_folder, 'concat.pt'
-        )
+    self.file_names = glob.glob(
+        os.path.join(root_dir, self.curr_folder, '*.mp3')
     )
 
-    self.data_len = self.data_concat.shape[0]
+    self.data_len = len(self.file_names)
 
     self.mean_val = torch.load(os.path.join(root_dir, 'mean.pt'))
     self.std_val = torch.load(os.path.join(root_dir, 'std.pt'))
-
-    # normalizing the data
-    self.data_concat = torch.reshape(
-        (self.data_concat - self.mean_val)/self.std_val,
-        (self.data_len, 1, -1)
-    )
 
   def __getitem__(self, index):
     """
@@ -51,7 +48,12 @@ class MusicLoader(data.Dataset):
         tuple: (image, target) where target is index of the target class.
     """
 
-    return self.data_concat[index, :, :]
+    _, raw_audio = read_mp3(self.file_names[index])
+
+    # randomly select a portion from the raw_audio
+    start_idx = random.randint(0, raw_audio.size-self.snippet_size-1)
+
+    return ((torch.FloatTensor(raw_audio[start_idx:start_idx+self.snippet_size]) - self.mean_val)/self.std_val).reshape(1, 1, -1)
 
   def __len__(self):
     return self.data_len
